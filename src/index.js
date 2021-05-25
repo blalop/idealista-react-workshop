262d79786896d0eece07dbf1eef1f87f58809c84
Alejandro Blanco López <ablopez@idealista.com>
2021-05-25 13:29:45 +0200

    Greeting component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import Greeting from './components/Greeting';
+
+ReactDOM.render(<Greeting message="Hello world"/>, document.querySelector('#root'));
